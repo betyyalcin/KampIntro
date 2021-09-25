aded8e1399ba4611453af001479f6d692c87a5f6
betyyalcin <betyyalcin20@gmail.com>
2021-09-25 17:03:09 +0300

    b

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //ürünle ilgili yapılacak olan operasyonların sınıfı.
+    class ProductManager
+    {
+        public void Add (Product product)
+        {
+            Console.WriteLine(product.ProductName + " ÜRÜN EKLENDİ.");   
+        }
+        public void Update (Product product)
+        {
+            Console.WriteLine(product.ProductName + " ÜRÜN GÜNCELLENDİ.");
+        }
+
+    }
+}
