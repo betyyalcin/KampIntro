d12558a9b6e7597c662b130a8c6e23c8b5610ba2
betyyalcin <betyyalcin20@gmail.com>
2021-09-25 14:51:08 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using Metotlar;
+using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyatı = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyatı = 6;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (Urun urun in urunler) //var item in tanımlanacak olan method
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyatı);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+            }
+
+            Console.WriteLine("------------Methotlar------------");
+            
+            //instance (Örnek)
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut" ,"YeşilArmut" , 12, 11);
+            sepetManager.Ekle2("Elma" ,"YeşilElma" , 12, 14);
+            sepetManager.Ekle2("Karpuz", "YeşilKarpuz", 12, 20);
+            
+
+        }
+    }
+}
---
+++ KampIntro/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // naming convention
+        //syntax
+        public void Ekle (Urun urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepete Eklendi!");
+
+        }
+
+        public void Ekle2 (string urunAdi, string aciklama, double Fiyat, int stokAdeti )
+        {
+            Console.WriteLine(urunAdi + " Sepete Eklendi!");
+
+        }
+    }
+}
